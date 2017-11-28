5ea553462da089d6827841f8e7fd3dbb6b91e6f0
Ralf Bach <ralf@kafu.dk>
2017-11-28 06:02:22 +0100

    Initial version

--- meta.js
+++ meta.js
@@ -44,32 +44,6 @@ module.exports = {
       "type": "confirm",
       "message": "Install vue-router?"
     },
-    "lint": {
-      "type": "confirm",
-      "message": "Use ESLint to lint your code?"
-    },
-    "lintConfig": {
-      "when": "lint",
-      "type": "list",
-      "message": "Pick an ESLint preset",
-      "choices": [
-        {
-          "name": "Standard (https://github.com/standard/standard)",
-          "value": "standard",
-          "short": "Standard"
-        },
-        {
-          "name": "Airbnb (https://github.com/airbnb/javascript)",
-          "value": "airbnb",
-          "short": "Airbnb"
-        },
-        {
-          "name": "none (configure it yourself)",
-          "value": "none",
-          "short": "none"
-        }
-      ]
-    },
     "unit": {
       "type": "confirm",
       "message": "Set up unit tests"
@@ -95,16 +69,12 @@ module.exports = {
           "short": "noTest"
         }
       ]
-    },
-    "e2e": {
-      "type": "confirm",
-      "message": "Setup e2e tests with Nightwatch?"
     }
   },
   "filters": {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
+    "config/test.env.js": "unit",
     "build/webpack.test.conf.js": "e2e || (unit && runner === 'karma')",
     "test/unit/**/*": "unit",
     "test/unit/index.js": "unit && runner === 'karma'",
@@ -112,7 +82,6 @@ module.exports = {
     "test/unit/karma.conf.js": "unit && runner === 'karma'",
     "test/unit/specs/index.js": "unit && runner === 'karma'",
     "test/unit/setup.js": "unit && runner === 'jest'",
-    "test/e2e/**/*": "e2e",
     "src/router/**/*": "router"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -6,49 +6,23 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
-  env: {
-    browser: true,
-  },
-  {{#if_eq lintConfig "standard"}}
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
-  {{/if_eq}}
-  {{#if_eq lintConfig "airbnb"}}
-  extends: 'airbnb-base',
-  {{/if_eq}}
   // required to lint *.vue files
   plugins: [
     'html'
   ],
-  {{#if_eq lintConfig "airbnb"}}
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
+  env: {
+    browser: true,
   },
-  {{/if_eq}}
   // add your custom rules here
   'rules': {
-    {{#if_eq lintConfig "standard"}}
+    'eqeqeq': ['error', 'allow-null'],
+    'space-before-function-paren': ["error", { "anonymous": "never", "named": "never" }],
+    'spaced-comment': 0,
+    'semi': ["error", "always"],
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    {{/if_eq}}
-    {{#if_eq lintConfig "airbnb"}}
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    {{/if_eq}}
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
